e6cbe7513a230e4431d515fe1ecdc9570b073c84
Victor <Victor>
2026-02-10 20:51:06 +0200

    add command factory

---
+++ MartianRobots/CommandFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace MartianRobots
+{
+    public class CommandFactory
+    {
+        private static readonly Dictionary<char, ICommand> _commands = new()
+        {
+            { 'L', new TurnLeftCommand() },
+            { 'R', new TurnRightCommand() },
+            { 'F', new MoveForwardCommand() }
+        };
+
+        public static ICommand GetCommand(char c)
+        {
+            if (!_commands.TryGetValue(c, out var command))
+                throw new InvalidOperationException($"Unknown command: {c}");
+
+            return command;
+        }
+    }
+}
